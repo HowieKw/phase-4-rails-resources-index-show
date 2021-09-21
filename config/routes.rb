86007d51c49f98d131661bdbb9307c9c661c4b47
Howard Kwon <howie.ykwon70@gmail.com>
2021-09-21 11:01:50 -0400

    commit tree f687c0b41dcf8833f4c4b7d7a41734b7a846ccfd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
+
+
+  # This will enforce all RESTful conventions and 
+  # can choose to use ONLY specific routes
+  resources :birds, only: [:index, :show]
+
 end
